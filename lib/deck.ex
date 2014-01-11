30a51f2df60f5455858acdbe48b0b4c073288d94
Augie De Blieck Jr <augiedb@gmail.com>
2014-01-10 21:49:39 -0500

    Split up Game and Deck modules, tests

--- lib/deck.ex
+++ lib/deck.ex
@@ -43,7 +43,8 @@ defmodule Deck do
 
   def deal_card(deck) do
     {card_list_of_one, rest_of_deck} = Enum.split(deck, 1) 
-    single_card = List.flatten(card_list_of_one) |> Enum.first
+    ##single_card = List.flatten(card_list_of_one) |> Enum.first
+    single_card = Enum.first(card_list_of_one)
     {single_card, rest_of_deck}
   end
 
@@ -70,129 +71,6 @@ defmodule Deck do
 
   def describe_card(card) do
     card.describe
-  end
-
-end
-
-defmodule Game do
-
-  # This is where you start the game
-  def play_a_game do
-    deck = Deck.create() |> Deck.shuffle
-    {hand, deck} = Deck.deal_hand(deck, 5)
-    {discard, deck} = Deck.deal_card(deck)
-    results = take_turn(hand, discard, deck)
-    IO.puts "You #{results}"
-  end
-
-  def top_discard_card(discards) when length(discards) > 1 do
-    IO.puts "greater than one"
-    { _ignore, top_card } = Enum.split(discards, -1)
-    Enum.first top_card    
-  end
-
-  def top_discard_card(discards) do
-    IO.puts "not greater than one"
-     discards
-  end
-
-  def play_card(card, hand, discard) do
-    hand = List.flatten hand
-    Game.show_hand(hand)
-    hand_new = hand -- [card]
-    discard_new = [discard] ++ [card]
-    {discard_new, hand_new}
-  end
-
-  def show_hand(list) do
-    show_hand(list, 1)
-  end
-  
-  defp show_hand([], _) do
-    #Nothing
-  end
-
-  defp show_hand([head|tail], acc) do
-    IO.puts "#{acc}: #{Deck.describe_card(head)}"
-    show_hand(tail, acc + 1)
-  end
-
-  def hand_value(hand) do
-  IO.puts "------------------"
-  Game.show_hand(hand)
-    points_list = Enum.map(hand, fn(x) -> x.points end)
-    Enum.reduce(points_list, 0, fn(x, acc) -> x + acc end)
-
-#    Enum.reduce(hand, 0, fn(x, acc) -> acc + x.points end)
-  end
-
-#--------- TAKE_TURNS
-
-
-
-  def take_turn(hand, discard, []) do
-    # TO DO: Problem: This will end the game if the user picks up the last card.
-    #        He or she won't have a chance to play the card if it IS a match.
-    #        Need to work on that logic.
-
-    # The draw is empty, but the user likely just picked up the last 
-    # card and should be allowed to play it, if possible.
-    # If it doesn't match, no harm. Just skip over this and end the game.
-    card_to_match = Game.top_discard_card(discard)
-    card_to_play = Deck.is_a_match(hand, card_to_match)
-    if card_to_play.suit != nil do  # MATCH
-      IO.puts "#{Deck.describe_card(card_to_match)} MATCHES #{Deck.describe_card(card_to_play)}"
-      {new_discard, new_hand } = play_card(card_to_play, hand, discard)
-      take_turn(new_hand, new_discard, [])
-    else
-      IO.puts "Length of hand list: #{length(hand)}"
-    
-      # TO DO: Include number of points held in losing hand
-      "Lose"
-    end
-  end
-
-  def take_turn([], _discard, []) do
-    # User played last remaining cards against discards stack
-    # when draw stack was already empty.
-    "Win"
-  end
-
-  def take_turn([], _discard, _draw) do
-    # Still cards left on draw stack, but user's hand is empty
-    "Win" 
-  end
-
-
-  def take_turn(hand, discard, draw) do
-    # Play a card in user's hand that matches the discard
-    # If a rank or value matches or you have a Crazy Eight, play the card
-    card_to_match = Game.top_discard_card(discard)
-    IO.puts "CARD TO MATCH: #{Deck.describe_card(card_to_match)}"
-    # Have to wrap discard up in brackets to treat it as a list.  MAGIC!
-    IO.puts "NUMBER OF CARDS IN THE DISCARD PILE: #{length([discard])}"
-    IO.puts "NUMBER OF CARDS IN THE DRAW    PILE: #{length(draw)}"
-    IO.puts "You have #{length(hand)} cards in your hand."
-    Game.show_hand(hand)
-
-    card_to_play = Deck.is_a_match(hand, card_to_match)
-
-    if card_to_play.suit == nil do  # NO MATCH
-      IO.puts "#-------------#"
-      IO.puts Deck.describe_card(card_to_match)
-      IO.puts "Matches NOTHING"
-      {card, new_draw} = Deck.deal_card(draw)
-      IO.puts "Took 1 from draw, added 1 to hand"
-      take_turn(hand ++ [card], discard, new_draw)
-    else
-      IO.puts Deck.describe_card(card_to_match)
-      IO.puts "Matches"
-      IO.puts Deck.describe_card(card_to_play)
-      {new_discard, new_hand} = play_card(card_to_play, hand, discard)
-
-      take_turn(new_hand, new_discard, draw)   
-    end
-
   end
 
 end
--- test/deck_test.exs
+++ test/deck_test.exs
@@ -1,6 +1,15 @@
 defmodule DeckTest do
   use ExUnit.Case
 
+
+  setup do
+    {:ok, deck: Deck.create() }
+  end
+
+  test "the truth", meta do
+    assert length(meta[:deck]) == 52 
+  end
+
   test "52 cards in a deck" do
     assert ( length(Deck.create()) == 52 )
   end
@@ -15,16 +24,17 @@ defmodule DeckTest do
     assert( Deck.init_points('King')  == 10)
   end
 
-  test "13 Heart Cards in a deck" do
-    deck = Deck.create()
+  test "13 Heart Cards in a deck", meta do
+    deck = meta[:deck]
     assert( Deck.count_suit(deck, 'Hearts') == 13 )
     assert( Deck.count_suit(deck, 'Clubs') == 13 )
     assert( Deck.count_suit(deck, 'Diamonds') == 13 )
     assert( Deck.count_suit(deck, 'Spades') == 13 )
   end
 
-  test "4 of each number in a deck" do
-    deck = Deck.create()
+  test "4 of each number in a deck", meta do
+   # deck = Deck.create()
+    deck = meta[:deck]
     assert( Deck.count_rank(deck, 9)      == 4)
     assert( Deck.count_rank(deck, 5)      == 4)
     assert( Deck.count_rank(deck, 'Jack') == 4)
@@ -55,87 +65,5 @@ defmodule DeckTest do
     assert( length(new_hand) == length(hand) + 1 )
     assert( length(new_draw) == length(draw) - 1 )
   end
-
-end
-
-
-defmodule GameTest do
-  use ExUnit.Case
-
-  test "Game Over/Win if hand is empty" do
-    d = Deck.create()
-    assert( Game.take_turn([], d, d) == "Win")
-  end
-
-  test "Game Over/Lose if draw is empty before the player's hand" do
-    d = Deck.create()
-    assert( Game.take_turn(d, d, []) == "Lose")
-  end
-
-  test "Show correct top card on discard stack" do
-    deck = Deck.create()
-    {discard, deck} = Deck.deal_card(deck)
-    top_card = Game.top_discard_card(discard)
-    assert( top_card == discard ) # not empty set
-
-    {new_card, _draw} = Deck.deal_card(deck)
-    new_discards = Game.top_discard_card([discard] ++ [new_card])
-    assert( new_discards == new_card )
-  end
-
-  test "Find a hand's points total" do
-    c_1 = Card.new rank: 'Jack', suit: 'Clubs', points: 10
-    c_2 = Card.new rank: 5,      suit: 'Hearts', points: 5
-    
-    hand = [c_1, c_2]
-    assert( Game.hand_value(hand) == 15 )
-  end
-
-  # This is one ridiculously long test, checking way too much stuff along the way.
-  # Like most such testing, it's done to help ME feel comfortable with my own code
-  # and with Elixir's libraries and functionalities.  At some point down the road, 
-  # I'll likely delete a chunk of this.
-  test "Play a card successfully" do
-    draw = Deck.create()
-    {hand, draw} = Deck.deal_hand(draw, 5)
-    assert(length(hand)    == 5         )
-    assert(length(draw)    == 52 - 5    )
-    {discard, draw} = Deck.deal_hand(draw, 1)
-    assert(length(draw)    == 52 - 5 - 1)
-    assert(length(discard) == 1         )
-    {hand, draw} = Deck.pick_up_card(draw, hand)
-    assert(length(hand)    == 6)
-    assert(length(draw)    == 52 - 5 - 1 - 1 )
-    card = Card.new rank: 'Jack', suit: 'Diamonds'
-    hand = hand ++ [card] # Hacky, but want to test that a specific card can be played
-    assert(length(hand) == 7)
-    {discard, hand} = Game.play_card(card, hand, discard)
-    assert(Enum.member?(discard, card) )
-    assert(length(discard) == 2)
-    assert(Enum.member?(hand, card) == :false )
-    assert(length(hand)    == 6)
-  end
-
-  test "Find a correct match to a rank and suit" do
-    c_1 = Card.new rank: 'Jack', suit: 'Clubs'
-    c_2 = Card.new rank: 5,      suit: 'Hearts'
-    hand = [c_1, c_2]
-    c_rank = Card.new rank: 'Jack', suit: 'Diamonds'
-    assert( Deck.is_a_match(hand, c_rank) == c_1 )
-    c_suit = Card.new rank: 'Ace', suit: 'Clubs'
-    assert( Deck.is_a_match(hand, c_suit) == c_1 )
-    c_suit = Card.new rank: 'Ace', suit: 'Diamonds'
-    assert( Deck.is_a_match(hand, c_suit) == Card.new )
-  end
-
-#  test "Take a turn with one card" do
-#    draw = Deck.create()
-#    discard = Deck.deal_hand(draw, 1)
-#    {hand, draw} = Deck.deal_hand(draw, 5)
-#    draw = Deck.deal_hand(draw, 1)
-#    {new_hand, new_draw} = Deck.pick_up_card(draw, hand)
-#    Game.take_turn(new_hand, discard, new_draw)
-#  end
-
 
 end
