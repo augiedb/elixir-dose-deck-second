92e5243aec1c5f98052e3c38da4450cfd66f63df
Augie De Blieck Jr <augiedb@gmail.com>
2014-01-14 21:13:54 -0500

    Clean up end game - now returns points value of the hand when you lose.

--- lib/game.ex
+++ lib/game.ex
@@ -16,6 +16,12 @@ defmodule Game do
 
 #--------- TAKE_TURNS
 
+  def take_turn([], _discard, []) do
+    # User played last remaining cards against discards stack
+    # when draw stack was already empty.
+    "Win"
+  end
+
   def take_turn(hand, discard, []) do
     # TO DO: Problem: This will end the game if the user picks up the last card.
     #        He or she won't have a chance to play the card if it IS a match.
@@ -24,25 +30,26 @@ defmodule Game do
     # The draw is empty, but the user likely just picked up the last 
     # card and should be allowed to play it, if possible.
     # If it doesn't match, no harm. Just skip over this and end the game.
+    IO.puts "Draw pile is now empty.  This is the end game portion."
+
     card_to_match = Game.see_top_discard_card(discard)
+    IO.puts "HELLO"
     card_to_play = Deck.is_a_match(hand, card_to_match)
+    IO.puts "You are trying to match the " <> card_to_match.describe <> " with your hand:"
+    Game.show_hand(hand)
     if card_to_play.suit != nil do  # MATCH
       IO.puts card_to_match.describe <> " MATCHES " <>  card_to_play.describe
       {new_discard, new_hand } = play_card(card_to_play, hand, discard)
+      IO.puts "The hand might be empty here and thus call a different iteration on take_turn/3."
+      Game.show_hand(new_hand)
+
       take_turn(new_hand, new_discard, [])
     else
-      #IO.puts "Length of hand list: #{length(hand)}"
-    
-      # TO DO: Include number of points held in losing hand
-      "Lose"
+      IO.puts "Length of hand list: #{length(hand)}"
+      total_points = hand_value(hand)
     end
   end
 
-  def take_turn([], _discard, []) do
-    # User played last remaining cards against discards stack
-    # when draw stack was already empty.
-    "Win"
-  end
 
   def take_turn([], _discard, _draw) do
     # Still cards left on draw stack, but user's hand is empty
@@ -75,9 +82,7 @@ defmodule Game do
       {new_discard, new_hand} = play_card(card_to_play, hand, discard)
 
       take_turn(new_hand, new_discard, draw)   
-      IO.puts "----"
     end
-    " ARE ENDING IN THE WRONG SPOT. Jerk."
   end
 
 
